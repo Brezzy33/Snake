909a839ed2aa8d0607352b46f90852276f9091fc
BreZzy33 <kenni1@live.ru>
2017-03-20 19:29:43 +0700

    Create Line

---
+++ Snake/Snake/HorLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class HorLine
+    {
+        List<Point> pList;
+
+        public HorLine( int xLeft, int xRight, int x, char sym)
+        {
+            pList = new List<Point>();
+            for (int y = xLeft; y <= xRight; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+
+        public void Draw()
+        {
+            foreach(Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
